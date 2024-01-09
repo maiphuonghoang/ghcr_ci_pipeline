e99930eeb1487975b28dbbddf1188d43c571f529
bhordupur <bhordupur@gmail.com>
2024-01-09 08:55:32 -0500

    Deploy django to local kubernetes

--- django/webapp/youtube/views.py
+++ django/webapp/youtube/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("Hello Viewers! We are testing how traffic is split")
+    return HttpResponse("This is a test? Viewers! We are testing how traffic is split")
